b304888292a307edb4ee51f169a904a1c4189aed
ShashithaU <udeshshashitha@gmail.com>
2024-12-22 00:00:02 +0530

    Developed Basic Controoler class

--- src/main/java/lk/icet/crm/model/ProductResponse.java
+++ src/main/java/lk/icet/crm/model/ProductResponse.java
@@ -1,4 +1,22 @@
 package lk.icet.crm.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Builder
+@Data
+@NoArgsConstructor
 public class ProductResponse {
+    private Integer id;
+    private String name;
+    private String description;
+    private Long price;
+    private String pictureUrl;
+    private String productBrand;
+    private String productType;
+
+
 }
