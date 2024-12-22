c63e326d80a86be90432a9c0b5e655952a22beab
ShashithaU <udeshshashitha@gmail.com>
2024-12-22 10:41:43 +0530

    Controller new commit

--- src/main/java/lk/icet/crm/controller/ProductController.java
+++ src/main/java/lk/icet/crm/controller/ProductController.java
@@ -1,10 +1,73 @@
 package lk.icet.crm.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.icet.crm.model.ProductResponse;
+import lk.icet.crm.service.BrandService;
+import lk.icet.crm.service.ProductService;
+import lk.icet.crm.service.TypeService;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.converters.models.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/products")
+@RequiredArgsConstructor
 public class ProductController {
+
+
+    private final ProductService productService;
+    private final BrandService brandService;
+    private final TypeService typeService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> getProduct(@PathVariable Integer id){
+        ProductResponse product = productService.getProduct(id);
+        return new ResponseEntity<>(productService.getProduct(id), HttpStatus.OK);
+    }
+
+    @GetMapping()
+    public ResponseEntity<Page<ProductResponse>> getProducts(
+            @RequestParam(name = "page", defaultValue = "0") int page,
+            @RequestParam(name = "size", defaultValue = "10") int size,
+            @RequestParam(name = "keyword", required = false) String keyword,
+            @RequestParam(name = "brandId", required = false) Integer brandId,
+            @RequestParam(name = "typeId", required = false) Integer typeId,
+            @RequestParam(name = "sort", defaultValue = "name") String sort,
+            @RequestParam(name = "order", defaultValue = "asc") String order
+    ){
+        //Convert order to Sort direction
+        Sort.Direction direction = order.equalsIgnoreCase("desc") ? Sort.Direction.DESC: Sort.Direction.ASC;
+        Sort sorting = Sort.by(direction, sort);
+        Pageable pageable = PageRequest.of(page, size, sorting);
+
+        Page<ProductResponse> productResponses = productService.getProducts(pageable, brandId, typeId, keyword);
+        return new ResponseEntity<>(productResponses, HttpStatus.OK);
+    }
+
+    @GetMapping()
+    public ResponseEntity<Page<ProductResponse>> getProducts(
+            @RequestParam(name = "page", defaultValue = "0") int page,
+            @RequestParam(name = "size", defaultValue = "10") int size,
+            @RequestParam(name = "keyword", required = false) String keyword,
+            @RequestParam(name = "brandId", required = false) Integer brandId,
+            @RequestParam(name = "typeId", required = false) Integer typeId,
+            @RequestParam(name = "sort", defaultValue = "name") String sort,
+            @RequestParam(name = "order", defaultValue = "asc") String order
+    ){
+        //Convert order to Sort direction
+        Sort.Direction direction = order.equalsIgnoreCase("desc") ? Sort.Direction.DESC: Sort.Direction.ASC;
+        Sort sorting = Sort.by(direction, sort);
+        Pageable pageable = PageRequest.of(page, size, sorting);
+
+        Page<ProductResponse> productResponses = productService.getProducts(pageable, brandId, typeId, keyword);
+        return new ResponseEntity<>(productResponses, HttpStatus.OK);
+    }
+
+
+
 
 }
