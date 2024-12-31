de957e28fdd850a65acd59caea89749608f0af7b
ShashithaU <udeshshashitha@gmail.com>
2024-12-31 09:00:22 +0530

    basket feature second commit

--- src/main/java/lk/UOK/crm/controller/ProductController.java
+++ src/main/java/lk/UOK/crm/controller/ProductController.java
@@ -20,12 +20,16 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
-@RequiredArgsConstructor
 public class ProductController {
     private final ProductService productService;
     private final BrandService brandService;
     private final TypeService typeService;
 
+    public ProductController(ProductService productService, BrandService brandService, TypeService typeService) {
+        this.productService = productService;
+        this.brandService = brandService;
+        this.typeService = typeService;
+    }
 
     @GetMapping("/{id}")
     public ResponseEntity<ProductResponse> getProductById(@PathVariable("id")Integer productId){
