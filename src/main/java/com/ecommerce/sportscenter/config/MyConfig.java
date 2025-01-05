69d6cedd52d90c2c899b88d15729ed8d7119030c
ShashithaU <udeshshashitha@gmail.com>
2025-01-05 09:41:00 +0530

    common first commit

--- src/main/java/com/ecommerce/sportscenter/config/MyConfig.java
+++ src/main/java/com/ecommerce/sportscenter/config/MyConfig.java
@@ -1,8 +1,8 @@
 package com.ecommerce.sportscenter.config;
 
+import org.springframework.security.core.userdetails.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
