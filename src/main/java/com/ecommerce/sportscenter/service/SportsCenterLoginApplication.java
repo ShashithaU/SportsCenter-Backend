41309440876bfe5777d8bd821d501abb1cb76cad
thurunuym <marasinghe.ty@gmail.com>
2025-01-16 19:40:16 +0530

    login backend added

---
+++ src/main/java/com/ecommerce/sportscenter/service/SportsCenterLoginApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.sportscenter.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsCenterLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsCenterLoginApplication.class, args);
+	}
+
+}
