aca07697e1d161b3a74670d0ef7699e3eb8a51db
thurunuym <marasinghe.ty@gmail.com>
2025-03-07 01:47:44 +0530

    new exceptions

--- src/main/java/com/ecommerce/sportscenter/exceptions/ProductNotFoundException.java
+++ src/main/java/com/ecommerce/sportscenter/exceptions/ProductNotFoundException.java
@@ -1,5 +1,20 @@
 package com.ecommerce.sportscenter.exceptions;
 
-public class ProductNotFoundException extends RuntimeException{
-    public ProductNotFoundException(String message){super(message);}
+public class ProductNotFoundException extends RuntimeException {
+    
+    private final String errorCode;
+
+    public ProductNotFoundException(String message) {
+        super(message);
+        this.errorCode = "PRODUCT_NOT_FOUND";
+    }
+
+    public ProductNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+        this.errorCode = "PRODUCT_NOT_FOUND";
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
 }
